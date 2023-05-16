010724e0e83bc948fb00953874e44f38b83373ca
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2023-05-16 12:46:55 -0300

    finish frontend configuration

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/Button.tsx"
+
+export default function Home() {
+  return (
+    <div className="text-2xl font-bold uppercase w-20 h-20 bg-red-500 rounded m-10">
+      oi
+    </div>
+  )
+}
