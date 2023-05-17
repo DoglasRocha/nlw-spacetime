cc446c3f5f4a009e07df56e18668e7b126358993
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2023-05-17 15:25:46 -0300

    start mobile homescreen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamJuree_700Bold} from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from "./src/assets/bg-blur.png"
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamJuree_700Bold
+  })
+
+  if (!hasLoadedFonts) return null
+
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-gray-50 font-bold text-5xl">Open up App.js to start working on your app!</Text>
+    <ImageBackground 
+      source={blurBg} 
+      className="relative bg-gray-900 flex-1 items-center"
+      imageStyle={{ position: "absolute", left: "-100%"}}
+    >
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackround>
   );
 }
